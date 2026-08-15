44b2e8c3666071c5bc85d911ef2c17537c31eace
TOLA <bongtola618@gmail.com>
2026-08-15 19:48:50 +0700

    Add features table migration

--- database/migrations/2026_08_15_124525_create_site_settings_table.php
+++ database/migrations/2026_08_15_124525_create_site_settings_table.php
@@ -1,27 +1,16 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
+public function up(): void
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('site_settings', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    Schema::create('site_settings', function (Blueprint $table) {
+        $table->id();
+        $table->string('key', 100)->unique();
+        $table->text('valu')->nullable();
+        $table->timestamps();
+    });
+}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('site_settings');
-    }
-};
+public function down(): void
+{
+    Schema::dropIfExists('site_settings');
+}
