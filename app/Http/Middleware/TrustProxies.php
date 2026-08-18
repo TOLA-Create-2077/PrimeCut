ad14c173154f8867ddfc8e16905740f18351131e
TOLA <bongtola618@gmail.com>
2026-08-18 15:32:42 +0700

    ff

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,8 +1,17 @@
-<?php 
-protected $proxies = '*';
+<?php
 
-protected $headers = Request::HEADER_X_FORWARDED_FOR |
-                     Request::HEADER_X_FORWARDED_HOST |
-                     Request::HEADER_X_FORWARDED_PORT |
-                     Request::HEADER_X_FORWARDED_PROTO |
-                     Request::HEADER_X_FORWARDED_AWS_ELB;
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Fideloper\Proxy\TrustProxies as Middleware; // Or Illuminate\Http\Middleware\TrustProxies depending on Laravel version
+
+class TrustProxies extends Middleware
+{
+    protected $proxies = '*';
+
+    protected $headers = Request::HEADER_X_FORWARDED_FOR |
+                         Request::HEADER_X_FORWARDED_HOST |
+                         Request::HEADER_X_FORWARDED_PORT |
+                         Request::HEADER_X_FORWARDED_PROTO |
+                         Request::HEADER_X_FORWARDED_AWS_ELB;
+}
